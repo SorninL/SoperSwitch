2c75e2306679be2f5a8702094759bdbe053a745d
BuildTools <sornin.contact@gmail.com>
2020-07-22 00:10:58 +0200

    push with errors bedtime

--- src/main/java/com/sopernetwork/core/commands/Startgame.java
+++ src/main/java/com/sopernetwork/core/commands/Startgame.java
@@ -1,5 +1,6 @@
 package com.sopernetwork.core.commands;
 
+import com.sopernetwork.core.coreutils.LogUtils;
 import com.sopernetwork.core.game.Game;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,6 +10,7 @@ public class Startgame implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        LogUtils.GetLogger().info("pass0");
         Game.GetGame().StartGame();
         return true;
     }
--- src/main/java/com/sopernetwork/core/game/Game.java
+++ src/main/java/com/sopernetwork/core/game/Game.java
@@ -1,6 +1,7 @@
 package com.sopernetwork.core.game;
 
 import com.sopernetwork.core.Core;
+import com.sopernetwork.core.coreutils.LogUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
@@ -29,6 +30,7 @@ public class Game {
 
     public static Game GetGame() {
         if (Game.game == null) {
+            LogUtils.GetLogger().info("pass0.5");
             return new Game(Core.getInstance().getServer().getOnlinePlayers());
         } else {
             return Game.game;
@@ -36,46 +38,59 @@ public class Game {
     }
 
     public void StartGame() {
-        if (this.onlinePlayers.size() <= 1) return;
+        LogUtils.GetLogger().info("pass0.75");
+        //if (this.onlinePlayers.size() <= 1) return;
+        LogUtils.GetLogger().info("pass0.80");
         this.registerSchedulers();
     }
 
 
     public void registerSchedulers() {
-        List<Player> players = this.onlinePlayers;
-        List<Player> processedPlayer = new ArrayList<Player>();
+        LogUtils.GetLogger().info("pass0");
         List<Integer> ints = new ArrayList<Integer>();
         ints.add(45);
         Runnable teleportPlayers = () -> {
+            List<Player> players = this.onlinePlayers;
+            List<Player> processedPlayer = new ArrayList<Player>();
+            LogUtils.GetLogger().info("pass1");
             int timerLastTime = ints.get(0);
+            ints.set(0, timerLastTime-1);
             if (timerLastTime > 10) return;
+            LogUtils.GetLogger().info("pass2");
             if (timerLastTime <= 10 && timerLastTime !=0) {
+                LogUtils.GetLogger().info("pass3");
                 Core.getInstance().getServer().broadcastMessage("§cSwitch in " + timerLastTime + " s");
+                return;
             }
+            ints.set(0, 45);
+            LogUtils.GetLogger().info("pass4");
             for(int i = 0; i<players.size();i++) {
                 if (players.get(i).getGameMode() == GameMode.SPECTATOR) {
                     processedPlayer.add(players.get(i));
                     players.remove(i);
                 }
                 if ((i+1) >= players.size()) {
-                    for (int j = 0; j<players.size();j++) {
-                        Player initPlayer = players.get(j);
-                        if (initPlayer == null) continue;
-                        players.remove(j);
-                        int randomIndex = ThreadLocalRandom.current().nextInt(0, players.size() + 1);
-                        Player randomPlayer = players.get(randomIndex);
-                        players.remove(randomIndex);
-                        Location initPlayerLocation = initPlayer.getLocation();
-                        Location randomPlayerLocation = randomPlayer.getLocation();
-                        initPlayer.sendMessage("Switched with " + randomPlayer.getName());
-                        initPlayer.teleport(randomPlayerLocation);
-                        randomPlayer.sendMessage("Switched with " + initPlayer.getName());
-                        randomPlayer.teleport(initPlayerLocation);
-                    }
+                    if ()
+                        for (int j = 0; j<players.size();j++) {
+                            Player initPlayer = players.get(j);
+                            if (initPlayer == null) continue;
+                            players.remove(j);
+                            int randomIndex = ThreadLocalRandom.current().nextInt(0, players.size() + 1);
+                            LogUtils.GetLogger().info(randomIndex + " yeet");
+                            Player randomPlayer = players.get(randomIndex);
+                            players.remove(randomIndex);
+                            Location initPlayerLocation = initPlayer.getLocation();
+                            Location randomPlayerLocation = randomPlayer.getLocation();
+                            initPlayer.sendMessage("Switched with " + randomPlayer.getName());
+                            initPlayer.teleport(randomPlayerLocation);
+                            randomPlayer.sendMessage("Switched with " + initPlayer.getName());
+                            randomPlayer.teleport(initPlayerLocation);
+                            ints.set(0, 45);
+                        }
                 }
             }
         };
-        long timer = 20;
+        long timer = 5;
         this.repeatedTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(Core.getInstance(),teleportPlayers, 0,timer);
     }
 }
